5391ac89c9771c5825379376d2787d54a1939733
natelao <lao.nathan@yahoo.com>
2023-08-18 15:52:03 -0500

    setup WiFi and stub protocol

---
+++ esp8266/main/include/protocol.hpp
@@ -0,0 +1,13 @@
+#ifndef __PROTOCOL_HPP__
+#define __PROTOCOL_HPP__
+
+namespace PROTOCOL
+{
+    typedef struct
+    {
+        uint32_t message_id;
+        /// @todo
+    } PAYLOAD;
+}
+
+#endif
--- esp8266/main/main.ino
+++ esp8266/main/main.ino
@@ -1,14 +1,73 @@
+// UDP Ethernet Libraries
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include <user_interface.h>
+#include "include/protocol.hpp"
+
+// LED Controller Libraries
 #include "include/led_controller.hpp"
 
+// UDP Ethernet Variables
+/// @note TOP SECRET
+const char* ssid = "lao-mesh";
+const char* passwd = "11531Seneca!";
+WiFiUDP udp;
+
+// UDP Multicast Receive on 239.1.1.1:4000
+IPAddress m_ip(239,1,1,1);
+unsigned int m_port = 4000;
+/// @note this is more than necessary...
+char input_buffer[1024];
+
+// LED Controller Variables
 LED::Controller *controller = 0;
 
 void setup()
 {
+    // Serial output
+    /// @todo may be removed in future
+    Serial.begin(115200);
+    Serial.println();
+
+    // Setup network configuration
+    WiFi.mode(WIFI_STA); // Station Mode (network client)
+    wifi_set_sleep_type(NONE_SLEEP_T); // reference https://arduino.stackexchange.com/questions/39957/esp8266-udp-multicast-doesnt-receive-packets
+    WiFi.setOutputPower(0); // Set WiFi output power to off - only receive
+
+    // Connect to network
+    WiFi.begin(ssid, passwd);
+    Serial.print("Connecting to ");
+    Serial.print(ssid);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("Connnection Successful");
+    Serial.print("IP ADDRESS: ");
+    Serial.println(WiFi.localIP());
+
+    // Setup UDP Multicast Configuration
+    if (udp.beginMulticast(WiFi.localIP(), m_ip, m_port))
+    {
+        Serial.println("Multicast Configuration Successful");
+        Serial.print("UDP MULTICAST ADDRESS: ");
+        Serial.println(m_ip);
+        Serial.print("UDP MULTICAST PORT: ");
+        Serial.println(m_port);
+    }
+    else
+    {
+        Serial.println("Multicast Configuration Failed");
+    }
+
     // Create the LED Controller object
     controller = new LED::Controller(300);
-
+    /// @todo consider indicating red if WiFi configuration failed
 }
+
 void loop()
 {
+    /// @todo check for incoming packets, parse and send to the controller
     controller->render();
 }
